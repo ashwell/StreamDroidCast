cd15284bb1a229755fbb05889a19c9d509e12af8
Ryan <Ryan.Bogle@gmail.com>
2012-03-03 12:36:16 -0800

    TCPServer Second commit

--- StreamDroidCast/src/com/CS454/StreamDroidCast/StreamDroidCastActivity.java
+++ StreamDroidCast/src/com/CS454/StreamDroidCast/StreamDroidCastActivity.java
@@ -3,9 +3,12 @@ package com.CS454.StreamDroidCast;
 import android.app.Activity;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * 
@@ -17,7 +20,8 @@ import android.widget.Button;
 
 public class StreamDroidCastActivity extends Activity implements OnClickListener{
     
-	Button scan;
+	private Button start;
+	private EditText ipAddressET;
 	
 	/** Called when the activity is first created. */
     @Override
@@ -29,12 +33,34 @@ public class StreamDroidCastActivity extends Activity implements OnClickListener
         if( !wifi.isWifiEnabled() )
         	wifi.setWifiEnabled(true);
         
-        scan = (Button)this.findViewById(R.id.startScan);
+        start = (Button)this.findViewById(R.id.start);
+        ipAddressET = (EditText)this.findViewById(R.id.ipAddr);
         
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart) + source.subSequence(start, end) + destTxt.substring(dend);
+                    if (!resultingTxt.matches ("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) { 
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i=0; i<splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+            return null;
+            }
+        };
+        ipAddressET.setFilters(filters);
         
         // Once connected
-        scan.setOnClickListener(this);
-        scan.setClickable(true);
+        start.setOnClickListener(this);
+        start.setClickable(true);
         
     }
 
